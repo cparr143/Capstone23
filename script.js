1bf104ac364ea6ca57de33a8af1b73f4a2f31b29
Cierra Parr <cierra.parr@yahoo.com>
2023-12-01 12:00:23 -0500

    added express

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+var daysEl = document.getElementsById('days');
+var hoursEl = document.getElementsById('hours');
+var minutesEl = document.getElementsById('minutes');
+var secondsEl = document.getElementsById('seconds');
 
+function countdownTimer() {
+    const countdownTimer = new Date('12-01-2023').getTime()
+
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const interval = setInterval(() => {
+
+    const now = new Date().getTime()
+    const distance = countdownDate - now
+
+    daysEl.innerText = formatNumber(Math.floor(distace / day))
+    hoursEl.innerText = formatNumber(Math.floor(distace % day / hour))
+    minutesEl.innerText = formatNumber(Math.floor(distace % day / minute))
+    secondsEl.innerText = formatNumber(Math.floor(distace % day / second))
+    }, 1000)
+}
+
+function formatNumber(number) {
+    if(number <10) {
+        return '0' + number
+    }
+    
+    return number
+}
+
+countdownTimer()
