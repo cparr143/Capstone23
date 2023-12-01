9890b03ae6b74192af6b956ad5ec8ba1c2c593e3
Cierra Parr <cierra.parr@yahoo.com>
2023-12-01 00:21:58 -0500

    updated html styling

--- app.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#mobile-menu');
-let menuLinks= document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
